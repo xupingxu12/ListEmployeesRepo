93b553bc656a0471f4fb37983b458a5c7589cff6
Xuping Xu <xuredc166@gmail.com>
2021-05-09 23:22:18 -0700

    Add project files.

---
+++ ListEmployees/Controllers/EmployeesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ListEmployees.Models;
+using Newtonsoft.Json;
+
+namespace ListEmployees.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+        // GET: api/employees
+        [HttpGet]
+        public IHttpActionResult Get(string lastName = null, string firstName = null,
+                                   int? departmentID = null, int? subDepartmentID = null)
+        {
+            TestDBEntities _entity = new TestDBEntities();
+            return Ok(_entity.SelectEmployee(lastName, firstName, departmentID, subDepartmentID));
+        }
+
+    }
+}
---
+++ ListEmployees/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ListEmployees.Models;
+
+namespace ListEmployees.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(string lastName = null, string firstName = null,
+                                   int? departmentID = null, int? subDepartmentID = null)
+        {
+            lastName = (lastName == null || lastName.Trim() == "") ? null : lastName.Trim();
+            firstName = (firstName == null || firstName.Trim() == "") ? null : firstName.Trim();
+
+            TestDBEntities _entity = new TestDBEntities();
+            return View(_entity.SelectEmployee(lastName, firstName, departmentID, subDepartmentID));
+        }
+
+    }
+}
